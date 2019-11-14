bacb6570d78b9641f543e251104db6fc92fe8694
Eric Davis <davis.25811@gmail.com>
2019-11-13 23:42:06 -0500

    Reafactor Team CRUD views (remove Materialize & add Bulma)

--- resources/views/admin/teams/edit.blade.php
+++ resources/views/admin/teams/edit.blade.php
@@ -1,54 +1,55 @@
 @extends('layouts.admin')
 
 @section('content')
-    <a href="{{ route('admin.teams.index') }}" class="waves-effect waves-light btn mt10 mb10">
-        <i class="material-icons left ml-15">keyboard_arrow_left</i>back
-    </a>
 
-    <div class="row">
-        <div class="col m12">
-            <div class="card">
-                <div class="card-content">
-                    @if (isset($team) && $team)
-                        <div class="card-title">Edit Team</div>
-                        {!! Form::model($team, ['route' => ['admin.teams.update', $team], 'method' => 'PUT']) !!}
-                    @else
-                        <div class="card-title">Create New Team</div>
-                        {!! Form::open(['route' => ['admin.teams.store'], 'method' => 'POST']) !!}
-                    @endif
-                    <div class="mt25">
-                        <div class="row">
+<a class="button is-link mb10" href="{{ route('admin.teams.index') }}">
+    <i class="fa fa-chevron-left"></i>
+</a>
 
-                            <div class="input-field col s12">
-                                {!! Form::text('name', null, ['id' => 'name', 'class' => 'validate', 'required' => 'required']) !!}
-                                <label for="name">Name</label>
-                            </div>
+<div class="box">
+    <h1 class="title is-3">{{ isset($team) && $team ? $team->name : 'Create Team' }}</h1>
+    <hr>
 
-                            <div class="input-field col m6">
-                                {!! Form::text('abbreviation', null, ['id' => 'country', 'class' => 'validate', 'required' => 'required']) !!}
-                                <label for="abbreviation">Abbreviation</label>
-                            </div>
+    @if (isset($team) && $team)
+        {!! Form::model($team, ['route' => ['admin.teams.update', $team], 'method' => 'PUT']) !!}
+    @else
+        {!! Form::open(['route' => ['admin.teams.store'], 'method' => 'POST']) !!}
+    @endif
 
-                            <div class="input-field col m6">
-                                {!! Form::text('country', null, ['id' => 'country', 'class' => 'validate', 'required' => 'required']) !!}
-                                <label for="country">Country</label>
-                            </div>
+    <div class="column is-one-third-desktop">
+        <div class="field">
+            <label class="label" for="name">Name</label>
+            <div class="control">
+                {!! Form::text('name', null, ['id' => 'name', 'class' => 'input', 'required' => 'required']) !!}
+            </div>
+        </div>
 
-                        </div>
-                    </div>
-                    <div class="card-action pl0 pr0">
-                        <button class="btn waves-effect waves-light green" type="submit" name="action">
-                            Save
-                            <i class="material-icons right">send</i>
-                        </button>
-                        <button class="btn waves-effect waves-light red" type="reset">
-                            Reset
-                            <i class="material-icons right">settings_backup_restore</i>
-                        </button>
-                    </div>
-                    {!! Form::close() !!}
-                </div>
+        <div class="field">
+            <label class="label" for="abbreviation">Abbreviation</label>
+            <div class="control">
+                {!! Form::text('abbreviation', null, ['id' => 'abbreviation', 'class' => 'input', 'required' => 'required']) !!}
+            </div>
+        </div>
+
+        <div class="field">
+            <label class="label" for="country">Country</label>
+            <div class="control">
+                {!! Form::text('country', null, ['id' => 'country', 'class' => 'input', 'required' => 'required']) !!}
+            </div>
+        </div>
+
+        <br>
+        <div class="field is-grouped">
+            <div class="control">
+                <button class="button is-success">Submit</button>
+            </div>
+            <div class="control">
+                <button class="button is-danger is-light">Cancel</button>
             </div>
         </div>
     </div>
+
+    {!! Form::close() !!}
+</div>
+
 @endsection
--- resources/views/admin/teams/index.blade.php
+++ resources/views/admin/teams/index.blade.php
@@ -1,55 +1,36 @@
 @extends('layouts.admin')
 
 @section('content')
-    <div class="row">
-        <div class="col m12">
-            <div class="card">
-                <div class="card-content">
-                    <span class="card-title d-inline-block mt5">Manage Teams</span>
 
-                    <a href="{{ route('admin.teams.create') }}" class="btn-floating waves-effect waves-light right green">
-                        <i class="material-icons center-align">add</i>
-                    </a>
-                    <div class="clearfix"></div>
+<div class="box">
+    <h1 class="title is-2">Manage Teams</h1>
+    <a class="button is-success is-rounded" href="{{ route('admin.teams.create') }}">
+        Create Team&nbsp;<i class="fa fa-plus"></i>
+    </a>
+    <hr>
+    <table class="table is-striped">
+        <thead>
+            <tr>
+                <th>Name</th>
+                <th>Abbr.</th>
+                <th>Country</th>
+                <th>Actions</th>
+            </tr>
+        </thead>
+        <tbody>
+        @foreach ($teams as $team)
+            <tr>
+                <td>{{ $team->name }}</td>
+                <td>{{ $team->abbreviation }}</td>
+                <td>{{ $team->country }}</td>
+                <td>
+                    <a href="{{ route('admin.teams.edit', $team) }}"><i class="material-icons">edit</i></a>
+                    <delete-confirm item-id="{{ $team->id }}" delete-route="{{ route('admin.teams.destroy', $team) }}"></delete-confirm>
+                </td>
+            </tr>
+        @endforeach
+        </tbody>
+    </table>
+</div>
 
-                    <div class="mt10">
-                        <hr>
-                        <table class="responsive-table" id="teams">
-                            <thead>
-                            <tr>
-                                <th>Name</th>
-                                <th>Abbr.</th>
-                                <th>Country</th>
-                                <th>Actions</th>
-                            </tr>
-                            </thead>
-                            <tbody>
-                            @foreach ($teams as $team)
-                                <tr>
-                                    <td>{{ $team->name }}</td>
-                                    <td>{{ $team->abbreviation }}</td>
-                                    <td>{{ $team->country }}</td>
-                                    <td>
-                                        <a href="{{ route('admin.teams.edit', $team) }}"><i class="material-icons">edit</i></a>
-                                        <delete-confirm item-id="{{ $team->id }}" delete-route="{{ route('admin.teams.destroy', $team) }}"></delete-confirm>
-                                    </td>
-                                </tr>
-                            @endforeach
-                            </tbody>
-                        </table>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-@endsection
-
-@section('scripts')
-    @parent
-
-    <script>
-        var teams = new Vue({
-            el: '#teams',
-        });
-    </script>
 @endsection
