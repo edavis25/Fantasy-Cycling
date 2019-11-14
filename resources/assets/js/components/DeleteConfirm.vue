2aef0c00e99e115c3fab8b35464edb5fea396250
Eric Davis <davis.25811@gmail.com>
2019-11-13 23:42:39 -0500

    Refactor DeleteConfirm component (remove Materialize & add Bulma)

--- resources/assets/js/components/DeleteConfirm.vue
+++ resources/assets/js/components/DeleteConfirm.vue
@@ -1,9 +1,9 @@
 <template>
-    <form v-bind:action="deleteRoute" v-bind:id="'delete-form-' + this.itemId" method="POST" class="d-inline">
+    <form v-bind:action="deleteRoute" v-bind:id="'delete-form-' + itemId" method="POST" class="d-inline">
         <input type="hidden" name="_token" v-bind:value="csrfToken" />
         <input type="hidden" name="_method" value="DELETE" />
-        <i class="material-icons red-text pl5 delete-btn pointer" @click="confirmDelete()">delete</i>
-        <div v-show="this.confirm" class="blue-text">
+        <i class="material-icons has-text-danger" @click="confirmDelete()">delete</i>
+        <div v-show="confirm" class="blue-text">
             <span class="pointer" @click="deleteItem()">yes</span>
             /
             <span class="pointer" @click="cancelDelete()">no</span>
