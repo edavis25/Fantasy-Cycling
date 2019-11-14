cfd3c667469048ddca0622caa711d4ae97259240
Eric Davis <davis.25811@gmail.com>
2019-11-13 23:12:04 -0500

    Refactor and cleanup admin layout (remove Materialize add Bulma)

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -1,7 +1,10 @@
 @extends('layouts.admin')
 
 @section('content')
-    <div class="row">
-        DASHBOARD FOR ADMIN!
+    <div class="box">
+        <h1 class="title is-2">Admin dashboard!</h1>
+        <p>
+            Not much going on here, check back later...
+        </p>
     </div>
 @endsection
--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -1,37 +1,45 @@
 @extends('layouts.master')
 
 @section('body')
-    <div class="admin">
-        @section('navbar')
-            @include('layouts.admin-sidebar')
-        @show
 
-        {{-- Show flash data --}}
-        <div class="container admin-content" id="app">
-            <div class="row">
-                <div class="col s12" id="alerts">
-                    @if (Session::has('success'))
-                        <alert type="success" message="{{ Session::get('success') }}"></alert>
-                    @elseif (Session::has('warning'))
-                        <alert type="warning" message="{{ Session::get('warning') }}"></alert>
-                    @elseif (Session::has('info'))
-                        <alert type="info" message="{{ Session::get('info') }}"></alert>
-                    @elseif (Session::has('error'))
-                        <alert type="danger" message="{{ Session::get('error') }}"></alert>
-                    @endif
-                </div>
-            </div>
-            @yield('content')
-        </div>
+<div class="admin">
+    <nav class="admin__sidebar">
+        <h1 class="admin__sidebarHeader">Admin</h1>
+        <ul>
+            <li class="admin__sidebarLink --header">General</li>
+            <li class="admin__sidebarLink">
+                <a href="{{ route('admin.races.index') }}">Races</a>
+            </li>
+            <li class="admin__sidebarLink">
+                <a href="{{ route('admin.teams.index') }}" class="white-text">Teams</a>
+            </li>
+            <li class="admin__sidebarLink">
+                <a href="{{ route('admin.riders.index') }}" class="white-text">Riders</a>
+            </li>
+            <li class="admin__sidebarLink --header">Races</li>
+            @foreach ($all_races as $race)
+                <li class="admin__sidebarLink --race">
+                    <a href="{{ route('admin.races.show', $race) }}">{{ $race->name }} - {{ $race->year }}</a>
+                </li>
+            @endforeach
+        </ul>
+    </nav>
+
+    <div class="admin__content">
+        {{-- FLASH MESSAGES --}}
+        @if (Session::has('success'))
+            <alert type="success" message="{{ Session::get('success') }}"></alert>
+        @elseif (Session::has('warning'))
+            <alert type="warning" message="{{ Session::get('warning') }}"></alert>
+        @elseif (Session::has('info'))
+            <alert type="info" message="{{ Session::get('info') }}"></alert>
+        @elseif (Session::has('error'))
+            <alert type="danger" message="{{ Session::get('error') }}"></alert>
+        @endif
+
+        {{-- MAIN ADMIN CONTENT --}}
+        @yield('content')
     </div>
-@endsection
-
-@section('scripts')
-    @parent
-    <script>
-        var alerts = new Vue({
-            el: '#alerts',
-        })
-    </script>
+</div>
 @endsection
 
