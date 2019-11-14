5c3191ff7aab00dea65fd9fdda13724ba9fdccc9
Eric Davis <davis.25811@gmail.com>
2019-11-13 22:50:37 -0500

    restyle login with Bulma

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -2,125 +2,53 @@
 
 @section('content')
 
-    <div class="row">
-        <div class="col l6 offset-l3">
-            <div class="card mt50">
+<div class="container">
+    <div class="columns is-multiline">
+        <div class="card is-6-desktop is-offset-3-desktop column">
+            <header class="card-header">
+                <h1 class="card-header-title is-size-4">Login</h1>
+            </header>
+            <div class="card-content">
                 <form method="POST" action="{{ route('login') }}" aria-label="{{ __('Login') }}">
                     @csrf
-                    <div class="card-content">
-                        <span class="card-title amber-text">Login</span>
-
-                        {{-- Show any errors --}}
-                        @if ($errors->has('email'))
-                            <span class="invalid-feedback red-text" role="alert">
-                                <strong>{{ $errors->first('email') }}</strong>
-                            </span>
-                        @endif
-                        @if ($errors->has('password'))
-                            <span class="invalid-feedback" role="alert">
-                                <strong>{{ $errors->first('password') }}</strong>
-                            </span>
-                        @endif
-
-                        <div class="input-field mt25">
-                            <input id="email" type="email" name="email" class="validate" value="{{ old('email') }}" required>
-                            <label for="email">Email</label>
+                    <div class="field">
+                        <label class="label">Email</label>
+                        <div class="control has-icons-left">
+                            <input class="input" type="email" name="email" value="{{ old('email') }}" required />
+                            <span class="icon is-small is-left"><i class="fa fa-envelope"></i></span>
                         </div>
-
-                        <div class="input-field">
-                            <input id="password" type="password" class="validate" name="password" required>
-                            <label for="password">Password</label>
-                        </div>
-
-                        <label>
-                            <input type="checkbox" name="remember" {{ old('remember') ? 'checked' : '' }}>
-                            <span>Remember Me</span>
-                        </label>
                     </div>
 
-                    <div class="card-action row">
-                        <button type="submit" class="btn waves-effect waves-light green">
-                            Login
-                        </button>
+                    <div class="field">
+                        <label class="label">Password</label>
+                        <div class="control has-icons-left">
+                            <input class="input" type="password" name="password" required />
+                            <span class="icon is-small is-left"><i class="fa fa-lock"></i></span>
+                        </div>
+                    </div>
 
-                        <a class="btn waves-effect waves-light amber right" href="{{ route('password.request') }}">
-                            Forgot Password
-                        </a>
-                        <div class="mt20">
-                            <a href="{{ route('register') }}" class="amber-text text-darken-3">Create an account</a>
+                    <div class="field">
+                        <div class="control">
+                            <label class="checkbox">
+                                <input type="checkbox" name="remember" {{ old('remember') ? 'checked' : '' }}>
+                                Remember me
+                            </label>
+                        </div>
+                    </div>
+
+                    <div class="field">
+                        <a href="{{ route('password.request') }}">Forgot password?</a><br>
+                        <div class="is-pulled-right">
+                            <input class="button is-success" type="submit" value="Login" />
                         </div>
                     </div>
                 </form>
             </div>
         </div>
-    </div>
-
-
-{{-- DEFAULT LARAVEL
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Login') }}</div>
-
-                <div class="card-body">
-                    <form method="POST" action="{{ route('login') }}" aria-label="{{ __('Login') }}">
-                        @csrf
-
-                        <div class="form-group row">
-                            <label for="email" class="col-sm-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ old('email') }}" required autofocus>
-
-                                @if ($errors->has('email'))
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $errors->first('email') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" name="password" required>
-
-                                @if ($errors->has('password'))
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $errors->first('password') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <div class="col-md-6 offset-md-4">
-                                <div class="checkbox">
-                                    <label>
-                                        <input type="checkbox" name="remember" {{ old('remember') ? 'checked' : '' }}> {{ __('Remember Me') }}
-                                    </label>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="form-group row mb-0">
-                            <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Login') }}
-                                </button>
-
-                                <a class="btn btn-link" href="{{ route('password.request') }}">
-                                    {{ __('Forgot Your Password?') }}
-                                </a>
-                            </div>
-                        </div>
-                    </form>
-                </div>
-            </div>
+        <div class="is-6-desktop is-offset-3-desktop column">
+            <a href="{{ route('register') }}">Create an account</a>
         </div>
     </div>
 </div>
---}}
+
 @endsection
